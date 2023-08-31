cd52e0dcdd4a6021e00dca57afd84a1f9d81cd45
rupesh <129289866+ru-pesh@users.noreply.github.com>
2023-08-31 17:25:41 +0545

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,5 +1,5 @@
 import random
-# random moduele is used to make computer choose a num
+# random moduele is used to make computer choose a numbe
 one=[' ','| ',' ','| ',' ']
 pr=['-+--+--']
 two=[' ','| ',' ','| ',' ']
